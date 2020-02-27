5b7e02bd1d7b6b507ce5b61ca17ceac65c4b1050
jaimelombanag <alombana24@hotmail.com>
2020-02-26 21:23:42 -0500

    se coloca el texto de la descripcion por una columna y todo por separado

--- proyectoplatzi/lib/description_place.dart
+++ proyectoplatzi/lib/description_place.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget{
+
+  final String descrip = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultrices arcu a mi interdum tempor. In consectetur at libero eget viverra. Mauris eget risus non nibh ultrices rhoncus. Aenean consectetur in orci quis efficitur. Aliquam vitae ante dolor. Nam volutpat felis dolor, vitae semper nunc rhoncus vel. Donec ac ante in nisi mollis facilisis a sit amet sapien. Curabitur accumsan tortor nec urna sollicitudin laoreet. Integer eu blandit elit. Pellentesque eget odio a nunc iaculis convallis.";
+
+
   @override
   Widget build(BuildContext context) {
+
+
+    final description = Container(
+      margin: EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0
+      ),
+      child: new Text(descrip,
+        style: TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
+        ),
+        textAlign: TextAlign.justify,
+      ),
+    );
+
+
 
     final star = Container(
       margin: EdgeInsets.only(
@@ -36,21 +59,27 @@ class DescriptionPlace extends StatelessWidget{
             textAlign: TextAlign.left,
           ),
         ),
+
         Row(
           children: <Widget>[
-            star,
-            star,
-            star,
-            star,
-            star
-          ],
-        )
+              star,
+              star,
+              star,
+              star,
+              star
+            ],
+          )
+        ],
+      );
 
+
+
+    return Column(
+      children: <Widget>[
+        title_stars,
+        description
       ],
     );
-
-
-    return title_stars;
   }
 
 
