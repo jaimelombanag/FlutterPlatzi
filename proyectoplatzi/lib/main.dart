7d28a5e061fd422fefc921aa94bcd6ec44604608
jaimelombanag <alombana24@hotmail.com>
2020-03-15 14:34:39 -0500

    se anaden imagenes de la parte superior

--- proyectoplatzi/lib/main.dart
+++ proyectoplatzi/lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
                   ReviewList()
                 ],
               ),
-              GradientBack()
+              GradientBack("Popular")
             ],
           )),
     );
